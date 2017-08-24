9272115273e3faf181da4d08c765b27f9dc22288
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2017-08-24 16:59:26 +0200

    ScancolSQ Format: update to new Location API

--- src/main/java/io/scif/formats/ScancoISQFormat.java
+++ src/main/java/io/scif/formats/ScancoISQFormat.java
@@ -40,7 +40,6 @@ import io.scif.Format;
 import io.scif.FormatException;
 import io.scif.ImageMetadata;
 import io.scif.config.SCIFIOConfig;
-import io.scif.io.RandomAccessInputStream;
 import io.scif.util.FormatTools;
 
 import java.io.IOException;
@@ -52,6 +51,9 @@ import java.time.ZoneOffset;
 import net.imagej.axis.Axes;
 import net.imagej.axis.DefaultLinearAxis;
 
+import org.scijava.io.handle.DataHandle;
+import org.scijava.io.handle.DataHandle.ByteOrder;
+import org.scijava.io.location.Location;
 import org.scijava.plugin.Plugin;
 
 /**
@@ -130,7 +132,7 @@ public class ScancoISQFormat extends AbstractFormat {
 		}
 
 		@Override
-		public boolean isFormat(final RandomAccessInputStream stream)
+		public boolean isFormat(final DataHandle<Location> stream)
 			throws IOException
 		{
 			final byte[] firstBytes = new byte[ISQ_ID.length()];
@@ -510,13 +512,13 @@ public class ScancoISQFormat extends AbstractFormat {
 	public static class Parser extends AbstractParser<Metadata> {
 
 		@Override
-		protected void typedParse(final RandomAccessInputStream stream,
+		protected void typedParse(final DataHandle<Location> stream,
 			final Metadata meta, final SCIFIOConfig config) throws IOException,
 			FormatException
 		{
 			config.imgOpenerSetComputeMinMax(true);
 
-			stream.order(true);
+			stream.setOrder(ByteOrder.LITTLE_ENDIAN);
 			stream.seek(28);
 			meta.setPatientIndex(stream.readInt());
 			meta.setScannerId(stream.readInt());
@@ -563,7 +565,7 @@ public class ScancoISQFormat extends AbstractFormat {
 			final ByteArrayPlane plane, final long[] planeMin, final long[] planeMax,
 			final SCIFIOConfig config) throws FormatException, IOException
 		{
-			final RandomAccessInputStream stream = getStream();
+			final DataHandle<Location> stream = getHandle();
 			final Metadata metadata = getMetadata();
 			final int offset = (int) (metadata.dataOffset + metadata.sliceBytes *
 				planeIndex);
